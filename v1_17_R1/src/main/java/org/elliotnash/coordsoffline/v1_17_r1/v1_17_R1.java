cc7c1a14046b57becbb1e915e38d225e5cb5230c
Elliot Nash <elliotnash@gmail.com>
2021-07-22 20:30:08 -0700

    use mojang mapped nms

--- v1_17_R1/src/main/java/org/elliotnash/coordsoffline/v1_17_r1/v1_17_R1.java
+++ v1_17_R1/src/main/java/org/elliotnash/coordsoffline/v1_17_r1/v1_17_R1.java
@@ -2,21 +2,20 @@ package org.elliotnash.coordsoffline.v1_17_r1;
 
 import com.mojang.authlib.GameProfile;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.level.EntityPlayer;
-import net.minecraft.server.level.PlayerInteractManager;
-import net.minecraft.world.level.World;
-import org.elliotnash.coordsoffline.nmsinterface.NmsManager;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.level.Level;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
+import org.elliotnash.coordsoffline.nmsinterface.NmsManager;
 
 
 public class v1_17_R1 implements NmsManager {
     public Player loadOfflinePlayer(OfflinePlayer player) {
         MinecraftServer minecraftserver = MinecraftServer.getServer();
         GameProfile gameprofile = new GameProfile(player.getUniqueId(), player.getName());
-        EntityPlayer entity = new EntityPlayer(
+        ServerPlayer entity = new ServerPlayer(
                 minecraftserver,
-                minecraftserver.getWorldServer(World.f),
+                minecraftserver.getLevel(Level.OVERWORLD),
                 gameprofile
         );
 
@@ -26,4 +25,3 @@ public class v1_17_R1 implements NmsManager {
         return target;
     }
 }
-
